0fca879e3b2714f462000f32e40f64a31b281ef1
Sam Minot <sminot@gmail.com>
2026-02-04 21:59:03 -0800

    Fix bug

--- .cirro/preprocess.py
+++ .cirro/preprocess.py
@@ -5,12 +5,13 @@ from cirro.helpers.preprocess_dataset import PreprocessDataset
 if __name__ == "__main__":
     # Get the information provided to run this process
     ds = PreprocessDataset.from_running()
+    ds.log()
 
     # Get the table of files given as inputs, filtering to
     # just those files with the .rds extension
     files = ds.files.loc[ds.files['file'].str.endswith('.rds')]
 
-    ds.log(f"Found {len(files):,} .rds files to process")
+    ds.logger.info(f"Found {len(files):,} .rds files to process")
 
     # Write out the table of files to be used as input to the workflow
     files.rename(columns={'file': 'rds'}).to_csv("samplesheet.csv", index=None)
